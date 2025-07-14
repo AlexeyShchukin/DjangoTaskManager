6d960ae15b2aa0576894479c449361c73107f05c
AlexeyShchukin <shchukin.developer@gmail.com>
2025-07-14 23:17:01 +0200

    feat: Add views and routes for tasks

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from src.app.views import hello
+from src.app.views import get_all_tasks, create_task, get_statistic, get_task_by_id
 
 urlpatterns = [
-    path('', hello)
+    path('tasks/', get_all_tasks),
+    path('tasks/create/', create_task),
+    path('tasks/statistic', get_statistic),
+    path('tasks/<int:task_id>/', get_task_by_id)
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,59 @@
-from django.http import HttpResponse
+from django.db.models import Count, Q
+from django.utils.timezone import now
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+from src.app.models import Task
+from src.app.models.task import Status
+from src.app.serializers import TaskSerializer
 
 
-def hello(request):
-    return HttpResponse("<h1>Hello, Alex.</h1>")
+@api_view(['GET'])
+def get_all_tasks(request: Request) -> Response:
+    response_data = Task.objects.all()
+    serializer = TaskSerializer(data=response_data, many=True)
+    if serializer.is_valid():
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+    return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_task_by_id(request: Request, task_id: int) -> Response:
+    task = get_object_or_404(Task, id=task_id)
+    serializer = TaskSerializer(task)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def create_task(request: Request) -> Response:
+    raw_data = request.data
+
+    serializer = TaskSerializer(data=raw_data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_statistic(request: Request) -> Response:
+    total_tasks = Task.objects.count()
+
+    tasks_by_status = Task.objects.values('status').annotate(count_by_status=Count('id'))
+
+    overdue_tasks = Task.objects.filter(
+        ~Q(status=Status.DONE),
+        deadline__lt=now()
+    ).count()
+
+    response_data = {
+        'total_tasks': total_tasks,
+        'tasks_by_status': {obj['status']: obj['count_by_status'] for obj in tasks_by_status},
+        'overdue_tasks': overdue_tasks
+    }
+
+    return Response(data=response_data, status=status.HTTP_200_OK)
